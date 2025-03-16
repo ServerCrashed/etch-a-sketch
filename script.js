6c65a2ea377fd4d56b45f14eb39e780e8b5e3df6
ServerCrashed <aks3045@proton.me>
2025-03-16 14:58:31 +0530

    Implement size modification function

--- script.js
+++ script.js
@@ -2,22 +2,39 @@ const container = document.querySelector("#container");
 
 console.log(`Height: ${container.offsetHeight}`);
 
-for(let i=0;i<16;i++){
-    let r = document.createElement("div");
-    r.style.border = "1px solid rgba(155, 154, 154, 0.253)";
-    r.style.width="100%";
-    r.style.height="100%";
-    container.appendChild(r);
-    for(let j=0; j<16;j++){
+function grid(n=16){
+    container.replaceChildren();
+    for(let i=0;i<n;i++){
+        let r = document.createElement("div");
+        r.style.border = "1px solid rgba(155, 154, 154, 0.253)";
+        r.style.width="100%";
+        r.style.height="100%";
+        container.appendChild(r);
         r.setAttribute("style","display:flex; flex-direction:row; justify-content:space-evenly")
-        let c = document.createElement("div");
-        c.classList.add("square");
-        // c.textContent="A";
-        r.appendChild(c);
+        for(let j=0; j<n;j++){        
+            let c = document.createElement("div");
+            c.classList.add("square");
+            c.style.width = `calc(960px/${n})`;
+            c.style.height = `calc(960px/${n})`;
+            r.appendChild(c);
+        }
     }
 }
+
 
 container.addEventListener('mouseover',(e)=>{
     const target = e.target;
     target.style.backgroundColor = "black";
 })
+
+const mod = document.querySelector("#size");
+mod.addEventListener('click',()=>{
+    let ns = prompt("Enter new grid size between 1 & 100.",16);
+
+    if(!(ns<=100 && ns>=1)) alert("Invalid size! Kindly note that 1 <= size <= 100");
+    else{
+        grid(ns);
+    }
+});
+
+grid();
